84a3c9e873cd47bb81837e4701ac79410c625e0b
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-06-02 23:20:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import  Header from './component/header'
+import Cart from './component/cart';
+import Main from './component/main';
 
 function App() {
   return (
     <>
       <Header/>
+      <Cart/>
+      <Main/>
     </>
   );
 }
