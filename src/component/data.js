c9f0219fc4b6761dcdc1994f03ad0f896d81eb38
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-06-02 23:20:31 +0200

    Create data.js

---
+++ src/component/data.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        compnany: 'sneaker company',
+        name:'Fall Limited Edition Sneakers',
+        discription:'These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer. ',
+        price: 250,
+        offre: 50,
+    }
+]
