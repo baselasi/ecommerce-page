d5e76c2b030a3d57dd7398085617e1103361bb9a
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-06-02 23:20:26 +0200

    Update header.js

--- src/component/header.js
+++ src/component/header.js
@@ -6,21 +6,24 @@ import cart from '../imgs/icon-cart.svg'
 
 export default function Header(){
     return(
+      
+    <div className="header-continer">
         <div className="header">
             <div className="navbar-left">
-                <div style={{backgroundImage: `url(${logo})` } } className="logo"></div>
+                <div style={{backgroundImage: `url(/imgs/logo.svg)` } } className="logo"></div>
                 <ul className="navalist">
-                    <li>Collections</li>
-                    <li>Men</li>
-                    <li>Women</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                    <li className="list-item"><a> Collections</a></li>
+                    <li className="list-item"><a> Menu</a></li>
+                    <li className="list-item"><a>Women</a></li>
+                    <li className="list-item"><a>About</a></li>
+                    <li className="list-item"><a>Contact</a></li>
                 </ul>
             </div>
             <div className="navbar-right">
-                <div style={{backgroundImage : `url(${cart})`}} className="cart"></div>
-                <div style={{backgroundImage : `url(${avatar})`}} className="avatar"></div>
+                <div style={{backgroundImage : `url(/imgs/icon-cart.svg)`}} className="cart"></div>
+                <div style={{backgroundImage : `url(/imgs/image-avatar.png)`}} className="avatar"></div>
             </div>
         </div>
+    </div>
     )
 }
