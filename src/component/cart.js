5a065fddc149424e86c6ed81c0d2e23190e23b0b
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-06-03 17:24:25 +0200

    Update cart.js

--- src/component/cart.js
+++ src/component/cart.js
@@ -1,19 +1,18 @@
 import React from "react";
 import './cart.css'
-
 export default function Cart(props){
     return(
-        <div className="modal">
-            <p style={{borderBottom: "1px solid", width:"100%"}} >cart</p>
-            <div>your cart is emty</div>
-            <div>
+        <div className="modal" style={{display: props.state ? "block" : "none"}}>
+            <p style={{borderBottom: "1px solid", width:"100%", fontWeight:"800"}} >cart</p>
+            <div style={{display: props.number===0 ? "block" : "none"}}>your cart is empty</div>
+            <div style={{display: props.number===0 ? "none":"block"}}>
                 <div style={{backgroundImage: `${props.img}`}}>
                 </div>
                 <div>
-                <p>{props.name}</p>
-                <p>${props.price}<span>{props.number}</span>${props.price*props.number}<span></span></p>
+                <p style={{fontWeight:"300"}}>{props.name}</p>
+                <p>${props.price}<span>  *{props.number}</span><span style={{fontWeight:"800"}}>  ${props.price*props.number}</span></p>
                 </div>
-                <button>Checkout</button>
+                <button className="checkout">Checkout</button>
             </div>
         </div>
     )
